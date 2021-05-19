e11cab7b3d1fe1624853b2b0aa69d148bcc23afd
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-05-19 14:26:42 -0400

    created tables

--- import_db.sql
+++ import_db.sql
@@ -1,30 +1,53 @@
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE users (
-id INTEGER PRIMARY KEY,
-fname varchar NOT NULL,
-lname varchar NOT NULL
+  id INTEGER PRIMARY KEY,
+  fname varchar NOT NULL,
+  lname varchar NOT NULL
 );
 
 CREATE TABLE questions (
-title TEXT PRIMARY KEY,
-body TEXT NOT NULL,
-associated_author varchar NOT NULL,
+  id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  associated_author varchar NOT NULL,
 
-FOREIGN KEY (associated_author) REFERENCES users(id)
+  FOREIGN KEY (associated_author) REFERENCES users(id)  
 );
 
 CREATE TABLE question_follows (
+  id INTEGER PRIMARY KEY,
+  question_id INTEGER NOT NULL , 
+  user_id INTEGER NOT NULL, -- id of the user following the question
 
-);
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+  FOREIGN KEY (user_id) REFERENCES user(id)
+); 
+
+/*
+  id  title     user_id
+  1    q1        1
+  2    q1        2
+  3    q2        1
+  4    q3        1   #questions should at least have 1 follower(the asker)
+  5   NULL       3   #didn't ask or follow any questions
+*/
 
 CREATE TABLE replies (
+  id INTEGER PRIMARY KEY,
+  subject_questions INTEGER NOT NULL,
+  parent TEXT,  --top level replies don't need parents
+  user_id INTEGER NOT NULL,
+  body TEXT NOT NULL,
 
+  FOREIGN KEY (parent) REFERENCES replies(id),
+  FOREIGN KEY (subject_questions) REFERENCES questions(id)
 );
 
 CREATE TABLE question_likes (
-title TEXT PRIMARY KEY,
-user_id INTEGER NOT NULL,
-FOREIGN KEY (title) REFERENCES questions(title),
-FOREIGN KEY (user_id) REFERENCES users(id)
+  question_id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
