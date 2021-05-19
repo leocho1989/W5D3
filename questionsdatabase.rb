c7dbde9991464e680d15f48f2fbde4955f2b845e
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-05-19 15:11:04 -0400

    creating question class

--- questionsdatabase.rb
+++ questionsdatabase.rb
@@ -1,7 +1,45 @@
 require 'sqlite3'
-require 'Singleton'
+require 'singleton'
 
 class QuestionsDatabase < SQLite3::Database
+  include Singleton
 
-include Singleton
+  def initialize
+    super('questions.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
 end
+
+class Question
+
+  attr_accessor :id, :title, :body, :associated_author
+
+  def self.all
+    data = QuestionsDatabase.instance.execute("SELECT * FROM questions")
+    data.map { |datum| Question.new(datum) }
+  end
+
+  def self.find_by_id(id)
+    question = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        id = ?
+    SQL
+
+    return nil if question.length < 0 # might be <= 0
+    Question.new(question.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @associated_author = options['associated_author']
+  end
+
+end
+
